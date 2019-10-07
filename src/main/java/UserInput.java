8c6e47215038f099a374b514017dc9adeaf2778a
danielle lane <daniellelane1821@gmail.com>
2019-10-07 10:53:31 -0500

    finished exercise and bonus

---
+++ src/main/java/UserInput.java
@@ -0,0 +1,45 @@
+import com.github.javafaker.Faker;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Something");
+        String input = sc.nextLine();
+        System.out.println("You entered: " + input);
+
+        if(!StringUtils.isNumeric(input)){
+            System.out.println(input + " is not a number");
+
+        }else {
+            System.out.println(input + " is a number");
+        }
+
+        System.out.println(StringUtils.swapCase(input));
+        System.out.println(StringUtils.reverse(input));
+
+///////////////////BONUS//////////////////////////////////////////////////////////////
+
+        Faker faker = new Faker();
+
+        String name = faker.name().fullName();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+
+        String streetAddress = faker.address().streetAddress();
+
+        System.out.println(name);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+
+
+    }
+
+
+
+}
